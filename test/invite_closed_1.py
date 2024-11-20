1d089880201aeed20a7ee968ac444b6d79fd2526
kschlesi <kschlesi42@gmail.com>
2024-11-19 23:53:02 -0800

    updates for 2024

---
+++ test/invite_closed_1.py
@@ -0,0 +1,35 @@
+import gmailer
+import pandas as pd
+
+cr = gmailer.GMailer(credentials_path='/var/autodraft/secrets')
+
+## get most recent sliding scale draft
+q = "You’re Invited - BFG Invite Tryout 5/12 _NAME_"
+draft_list = cr.list_drafts(q=q, maxResults=10)
+drafts = [cr.get_draft(d['id'], format='full') for d in draft_list['drafts']]
+
+print([draft['id']+draft['message']['snippet'] for draft in drafts])
+if len(drafts)>0:
+    draft = drafts[0]
+    template_id = draft['id']
+    print(template_id)
+else:
+    template_id = 'r5781599331753565967'
+
+# replace draft strings with variable info
+data = pd.read_csv('/Users/kimberly.schlesinger/Documents/codethings/autodraft/test/invite_closed_1.csv')
+for row in data.iterrows():
+    print(row[1]['FULLNAME'])
+    if row[1]['SEND'] == 'Yes':
+        replace_tuples = [
+            ('_NAME_',row[1]['NAME'])
+        ]
+        print(template_id)
+        print(replace_tuples)
+        print(row[1]['EMAIL'])
+        draft = cr.create_draft_from_template(
+            template_id=template_id
+            , replace_tuples=replace_tuples
+            , send_from='bfgtc2024@gmail.com'
+            , send_to=row[1]['EMAIL']
+        )
--- test/test_script.py
+++ test/test_script.py
@@ -4,7 +4,7 @@ import pandas as pd
 cr = gmailer.GMailer(credentials_path='/var/autodraft/secrets')
 
 ## get most recent sliding scale draft
-q = "from:kschlesi42@gmail.com sliding scale"
+q = "from:kschlesi42@gmail.com bfg round 1 dues payment"
 draft_list = cr.list_drafts(q=q, maxResults=10)
 drafts = [cr.get_draft(d['id'], format='full') for d in draft_list['drafts']]
 
@@ -13,17 +13,24 @@ if len(drafts)==1:
     template_id = draft['id']
     print(template_id)
 else:
-    template_id = 'r2408711609765372954'
+    template_id = drafts[0]['id'] #'r2408711609765372954'
 
 # replace draft strings with variable info
-data = pd.read_csv('/Users/kimberly.schlesinger/Documents/codethings/autodraft/test/raw_tiers.csv')
+data = pd.read_csv('/Users/kimberly.schlesinger/Documents/codethings/autodraft/test/raw_dues_round1_2024.csv')
+i = 1
 for row in data.iterrows():
-    print(row[1]['Fullname'])
-    replace_tuples = [('XNAMEX',row[1]['Name']), ('XTIERX', str(row[1]['Tier'])), ('XPCTX', str(row[1]['Percent']))]
-    draft = cr.create_draft_from_template(
-        template_id=template_id
-        , replace_tuples=replace_tuples
-        , send_from='kschlesi42@gmail.com'
-        , send_to=row[1]['Email']
-        , send_cc='crystal.koo@gmail.com'
-    )
+    if i==1:
+        print(row[1]['Fullname'])
+        replace_tuples = [('XXNAMEXX',row[1]['Name'])
+                          , ('XXAMOUNTXX', str(row[1]['Amount']))
+                          , ('XXNOUNXX', str(row[1]['Noun']))
+                          , ('XXPLACEHOLDERXX', '' if bool(row[1]['Send']) else 'XXPLACEHOLDERXX')
+                          ]
+        draft = cr.create_draft_from_template(
+            template_id=template_id
+            , replace_tuples=replace_tuples
+            , send_from='kschlesi42@gmail.com'
+            , send_to=row[1]['Email']
+            , send_cc='cherylhsu168@gmail.com'
+        )
+        # i = 0
